9804de8e4b13d4bda954924a5d554a94a2df0005
ayushblake <32408246+ayushblake@users.noreply.github.com>
2021-10-24 12:50:11 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'http://localhost:5000/posts'
+const url = 'https://express-memories-api.herokuapp.com/posts'
 
 export const fetchUrl = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
